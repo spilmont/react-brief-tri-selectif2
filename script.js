ec43788d07491eca4304cf32bb36871cdf5274be
spilmont <dd_spilmont@hotmail.fr>
2018-11-22 21:12:59 +0100

    probleme repetition des alerte -1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var Dechets = ["bouteille-plastique.jpg", "bouteille-verre.jpg", "boite-conserve.jpg", "lait.jpg", "pomme.jpg", "carton.jpg", "pot de yaourt", "bouteille de coca", "mouchoir papier", "gobelet en plastique"];
+var Dechets = ["bouteille en plastique", "bouteille en verre", "boite de conserve", "lait", "pomme", "carton", "yaourt", "canette de coca", "mouchoir en papier", "gobelet en plastique"];
 
 
 var Utilsateur = [];
@@ -18,24 +18,56 @@ var PoubelleMarron = [];
 for (var i = 0; i < Dechets.length; i++) {
     var dechet = document.createElement('img');
     dechet.id = "dechet" + i;
-    dechet.src = `images/${dechets[i]}`;
+    dechet.src = `images/${Dechets[i]}.jpg`;
+    dechet.title = Dechets[i];
     dechet.draggable = "true";
     dechet.style.width = `${100}px`;
     dechet.style.height = `${100}px`;
-    dechet.innerHTML = Dechets[i];
     document.getElementById('dechet-box').appendChild(dechet);
-
-
 }
 
-document.querySelector('#poubelle-jaune').addEventListener("drop", function (e) {
+
+
+document.querySelector('#poubelle-jaune').addEventListener("dragover", function (e) {
     e.preventDefault();
-
-
+});
+document.querySelector('#poubelle-bleu').addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
+document.querySelector('#poubelle-vert').addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
+document.querySelector('#poubelle-marron').addEventListener("dragover", function (e) {
+    e.preventDefault();
 });
 
-document.querySelector('#dechet0').addEventListener("dragover", function (e) {
-    e.preventDefault();
+for (var i = 0; i < Dechets.length; i++) {
+
+    document.querySelector('#dechet' + i).addEventListener("dragend", function (e) {
+        e.preventDefault();
+        var select = this.title;
+        var pJaune = document.querySelector('#poubelle-jaune');
+        var pBleu = document.querySelector("#poubelle-bleu");
+        var pVert = document.querySelector("#poubelle-vert");
+        var pMarron = document.querySelector("#poubelle-marron");
+        console.log(select);
 
 
-})
+       pJaune.addEventListener("drop", function (e) {
+           e.preventDefault();
+           alert(`${select} a été déposé dans la poubelle jaune`);
+       });
+        pBleu.addEventListener("drop", function (e) {
+            e.preventDefault();
+            alert(`${select} a été déposé dans la poubelle bleu`);
+        });
+        pVert.addEventListener("drop", function (e) {
+            e.preventDefault();
+            alert(`${select} a été déposé dans la poubelle vert`);
+        });
+        pMarron.addEventListener("drop", function (e) {
+            e.preventDefault();
+            alert(`${select} a été déposé dans la poubelle marron`);
+        });
+    })
+}
